d9152891855462abfc451052f1786b291b777d80
Your Name <manzoorahmadwayar@gmail.com>
2026-01-04 15:26:42 +0430

    Dasd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+
 /** @type {import('next').NextConfig} */
+
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest\.json$/],
+});
+
 const nextConfig = {
   reactStrictMode: true,
   
@@ -39,6 +51,89 @@ const nextConfig = {
       },
     ];
   },
+  
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()'
+          }
+        ]
+      }
+    ];
+  }
 };
 
-export default nextConfig;
+export default pwaConfig(nextConfig);
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+  
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'dr-shekari.com',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         pathname: '/**',
+//       },
+//       {
+//         protocol: 'http',
+//         hostname: '192.168.56.1',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+  
+//   compress: true,
+//   poweredByHeader: false,
+  
+//   async redirects() {
+//     return [
+//       {
+//         source: '/index',
+//         destination: '/',
+//         permanent: true,
+//       },
+//       {
+//         source: '/home',
+//         destination: '/',
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
---
+++ src/components/ServiceWorkerRegistration.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServiceWorkerRegistration = () => {
+  return (
+    <div>ServiceWorkerRegistration</div>
+  )
+}
+
+export default ServiceWorkerRegistration
