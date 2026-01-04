54645869938c50e4c8e9df7ebe0ca82fb0754c6e
Your Name <manzoorahmadwayar@gmail.com>
2026-01-04 19:32:04 +0430

    asd

--- next.config copy.mjs
+++ next.config copy.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   
   images: {
-    remotePatterns: [
+    remotePatterns: [ 
       {
         protocol: 'https',
         hostname: 'dr-shekari.com',
